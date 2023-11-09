38761358a0a1c94ce3685dee399c0bc10e9ce3e5
eliseherben <146279872+eliseherben@users.noreply.github.com>
2023-11-09 19:55:07 +0100

    Update income_us.py

--- income_us.py
+++ income_us.py
@@ -809,25 +809,25 @@ with tab3:
         
         bar45 = go.Figure()
 
-        bar45.add_bar(
+        bar45.add_trace(go.Bar(
             x=['Gemiddelde huishoudinkomen', 'Mediaan huishoudinkomen'],
             y=[gemiddelde_inkomen1, mediaan_inkomen1],
             error_y=dict(type='constant', value=standaarddeviatie_inkomen1),
             labels={'y': 'Inkomen'}, barmode = 'group',
             name = selected_county1,
             title='Gemiddelde, Mediaan en Standaarddeviatie van Inkomen'
-        )
+        ))
         
         verg_county2 = verg[verg['County'] == selected_county2]
         gemiddelde_inkomen2 = verg_county2.iloc[0, 1]
         standaarddeviatie_inkomen2 = verg_county2.iloc[0, 3]
         mediaan_inkomen2 = verg_county2.iloc[0, 2]
-        bar45.add_bar(
+        bar45.add_trace(go.Bar(
             x=['Gemiddelde huishoudinkomen', 'Mediaan huishoudinkomen'],
             y=[gemiddelde_inkomen2, mediaan_inkomen2],
             error_y=dict(type='constant', value=standaarddeviatie_inkomen2),
             name=selected_county2
-        )
+        ))
         st.plotly_chart(bar45)
 
 # In[ ]:
